164c90462d3cc0ffe2636e997fd0c8c82286e971
superfast852 <superfast852@gmail.com>
2023-04-08 23:13:30 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# Project L
+### A Competitive, Capable robot based on Mecanum and Lidar.
+
+---
+#### Table of Contents
+1. [Introduction](#introduction)
+2. [Hardware](#hardware)
+3. [Software](#software)
+4. [TODO](#todo)
+5. [Goals](#goals)
+
+---
+## Introduction
+L is a robot designed to compete in the 2019 VEX Robotics Competition. It is a 4 wheel drive robot with a 4 bar lift and a 4 bar intake. It is capable of autonomous and driver control. It is also capable of autonomous scoring of the 20 point cube.
+
+---
+## Hardware
+L is built around a 4 wheel drive chassis with 4 mecanum wheels. It has a 4 bar lift and a 4 bar intake. It has a 4 motor drive train and a 4 motor lift. It has a 2 motor intake. It has a 2 motor shooter. It has a 2 motor indexer. It has a 2 motor conveyor. It has a 2 motor intake. It has a 2 motor lift. It has a 2 motor drive tr
+
+---
+## Software
+L is programmed in PROS. It has a 2 motor drive train. It has a 2 motor lift. It has a 2 motor intake. It has a 2 motor shooter. It has a 2 motor indexer. It has a 2 motor conveyor. It has a 2 motor intake. It has a 2 motor lift. It has a 2 motor drive tr
+
+---
+## TODO:
+####    Drive:
+- [ ] Linking PCB Build
+- [ ] Get the damn parts
+
+####    Power:
+- [ ] Power Regulation and Distribution block: A block that takes in 12V and 
+outputs every power connection. Includes Battery voltage monitoring and cutoff. Uses an arduino to transmit data.
+
+####    Signals:
+- [ ] Encoder Handling
+- [ ] GPS Handling
+- [ ] Lidar Handling
+- [ ] MPU-9250 Integration
+- [ ] I2C Rail
+
+####    Software:
+- [ ] Sensor Interface
+- [ ] Proper Mapping
+- [ ] Autonomous
+- [ ] Low Level -> High Level Interface
+- [ ] A* Path Finding and Following
+- [ ] SLAM (See [BreezySLAM](https://github.com/simondlevy/breezyslam))
+- [ ] Robot Dashboard
+- [ ] Drive Integration
+- [ ] Arm Integration
+
+####    Expansion:
+- [ ] Find out how to enable expansion
+
+####    Comms:
+- [ ] Local Communication (Drive -> Pi <- PDB)
+- [ ] Remote Communication (Pi -> Driver Station)
+- [ ] Long-Term Data Storage (Maps, Logs, etc.)
+
+####    Base:
+- [ ] Get the damn base
+- [ ] Expandability Standard
+
+---
+## Goals
+- [ ] Automatic Navigation
+- [ ] Speed
+- [ ] A-B Navigation on prerecorded map
+- [ ] SLAM
+- [ ] Path Planning
+- [ ] Task Creation Framework (Component Interface)
+
+---
+## Links
+- [BreezySLAM]
+- [Parts List](https://docs.google.com/spreadsheets/d/1OO8v4pfx6eXCZQJqTUq6JUSEajtX1vfl/edit?usp=sharing&ouid=107364809967877055034&rtpof=true&sd=true)
---
+++ controller.py
@@ -0,0 +1,105 @@
+from inputs import get_gamepad
+from threading import Thread
+
+
+class XboxController(object):
+    MAX_TRIG_VAL = 1024
+    MAX_JOY_VAL = 32768
+
+    def __init__(self, deadzone=0.1):
+
+        self.deadzone = deadzone
+        self.found = False
+        self.LJoyY = 0
+        self.LJoyX = 0
+        self.RJoyY = 0
+        self.RJoyX = 0
+        self.LT = 0
+        self.RT = 0
+        self.LB = 0
+        self.RB = 0
+        self.A = 0
+        self.X = 0
+        self.Y = 0
+        self.B = 0
+        self.LJoyB = 0
+        self.RJoyB = 0
+        self.Back = 0
+        self.Start = 0
+        self.LD = 0
+        self.RD = 0
+        self.UD = 0
+        self.DD = 0
+
+        self._monitor_thread = Thread(target=self._monitor_controller, args=())
+        self._monitor_thread.daemon = True
+        self._monitor_thread.start()
+
+    def read(self):  # return the buttons/triggers that you care about in this methode
+        reads = [self.LJoyX, self.LJoyY, self.RJoyX, self.RJoyY,
+                 self.RT, self.A, self.Back, self.Start]
+
+        return [self._clean(i) for i in reads]
+
+    def _clean(self, x):
+        return round(x, 3) if not self.deadzone > x > -self.deadzone else 0
+
+    def _monitor_controller(self):
+        while True:
+            try:
+                events = get_gamepad()
+                self.found = True
+                for event in events:
+                    if event.code == 'ABS_Y':
+                        self.LJoyY = self._clean(-(event.state / XboxController.MAX_JOY_VAL))  # normalize between -1 and 1
+                    elif event.code == 'ABS_X':
+                        self.LJoyX = self._clean(event.state / XboxController.MAX_JOY_VAL)  # normalize between -1 and 1
+                    elif event.code == 'ABS_RY':
+                        self.RJoyY = self._clean(-(event.state / XboxController.MAX_JOY_VAL))  # normalize between -1 and 1
+                    elif event.code == 'ABS_RX':
+                        self.RJoyX = self._clean(event.state / XboxController.MAX_JOY_VAL)  # normalize between -1 and 1
+                    elif event.code == 'ABS_Z':
+                        self.LT = self._clean(event.state / XboxController.MAX_TRIG_VAL)  # normalize between 0 and 1
+                    elif event.code == 'ABS_RZ':
+                        self.RT = self._clean(event.state / XboxController.MAX_TRIG_VAL)  # normalize between 0 and 1
+                    elif event.code == 'BTN_TL':
+                        self.LB = event.state
+                    elif event.code == 'BTN_TR':
+                        self.RB = event.state
+                    elif event.code == 'BTN_SOUTH':
+                        self.A = event.state
+                    elif event.code == 'BTN_NORTH':
+                        self.X = event.state  # previously switched with X
+                    elif event.code == 'BTN_WEST':
+                        self.Y = event.state  # previously switched with Y
+                    elif event.code == 'BTN_EAST':
+                        self.B = event.state
+                    elif event.code == 'BTN_THUMBL':
+                        self.LJoyB = event.state
+                    elif event.code == 'BTN_THUMBR':
+                        self.RJoyB = event.state
+                    elif event.code == 'BTN_SELECT':
+                        self.Back = event.state
+                    elif event.code == 'BTN_START':
+                        self.Start = event.state
+                    elif event.code == 'ABS_HAT0Y':
+                        self.UD = event.state == -1
+                        self.DD = event.state == 1
+                    elif event.code == 'ABS_HAT0X':
+                        self.LD = event.state == -1
+                        self.RD = event.state == 1
+            except:
+                print("Controller not found.")
+                self.found = False
+
+    @staticmethod
+    def edge(pulse, last, rising=True):
+        status = (pulse if rising else not pulse) and pulse != last
+        return status, pulse
+
+
+if __name__ == '__main__':
+    joy = XboxController()
+    while True:
+        ins = joy.read()
+        print(ins)
