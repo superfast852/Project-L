c997cb6446a5d268c3fef6bcca12f0e7f7d907e3
superfast852 <superfast852@gmail.com>
2023-05-07 21:57:52 -0400

    Scrapped the whole packages folder and now im force installing things

--- installer.sh
+++ installer.sh
@@ -1,8 +1,8 @@
-cd Packages/BreezySLAM
-python3 setup.py install --user
-cd ../rrt
+git clone https://github.com/simondlevy/BreezySLAM.git
+cd BreezySLAM/python
 python3 setup.py install --user
 cd ../..
+sudo rm -rf BreezySLAM
 sudo apt install python3-numpy python3-matplotlib python3-scipy python3-pip libopenblas-dev gfortran
 pip install -r requirements.txt
-pip install --no-deps scikit-image
+pip install --no-deps scikit-image rrtplanner
