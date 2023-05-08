35536e551cb44c14e3d680eadd2d717bff4c8230
superfast852 <superfast852@gmail.com>
2023-05-07 21:49:27 -0400

    Added breezyslam and rrtplanner to deal with the scipy bug

--- installer.sh
+++ installer.sh
@@ -1,8 +1,8 @@
-git clone https://github.com/simondlevy/BreezySLAM.git
-cd BreezySLAM/python
+cd Packages/BreezySLAM
+python3 setup.py install --user
+cd ../rrtplanner
 python3 setup.py install --user
 cd ../..
-sudo rm -r BreezySLAM
 sudo apt install python3-numpy python3-matplotlib python3-scipy python3-pip libopenblas-dev gfortran
 pip install -r requirements.txt
 pip install --no-deps scikit-image
