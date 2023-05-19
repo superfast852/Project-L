b47333d7096180c119b25041f7734a9e9b93f2a3
superfast852 <superfast852@gmail.com>
2023-05-19 13:48:32 -0400

    Added the pinout for DualDrive Loyolabot

--- Robots/Loyola.py
+++ Robots/Loyola.py
@@ -4,30 +4,50 @@ from Robots.HAL import Drive, RP_A1, io as GPIO, time
 
 class LDrive(Drive):  # Override Communications
     def comms(self, com, baud, update_freq=10):
-        GPIO.setup(19, GPIO.OUT)
-        self._l_pwm = GPIO.PWM(19, 1000)
-        self._l_pwm.start(0)
-        GPIO.setup(6, GPIO.OUT)
-        self._r_pwm = GPIO.PWM(6, 1000)
-        self._r_pwm.start(0)
-        print("PWM Started.")
-        GPIO.setup(26, GPIO.OUT)
-        GPIO.setup(13, GPIO.OUT)
-        GPIO.output(26, 0)
-        GPIO.output(13, 0)
+        GPIO.setup(12, GPIO.OUT)
+        self._lf_pwm = GPIO.PWM(12, 1000)
+        self._lf_pwm.start(0)
 
-        self.left_dir = lambda x: GPIO.output(26, x)
-        self.left_speed = lambda x: self._l_pwm.ChangeDutyCycle(x)
-        self.right_dir = lambda x: GPIO.output(13, x)
-        self.right_speed = lambda x: self._r_pwm.ChangeDutyCycle(x)
-        print("INIT Done.")
+        GPIO.setup(13, GPIO.OUT)
+        self._rf_pwm = GPIO.PWM(13, 1000)
+        self._rf_pwm.start(0)
+
+        GPIO.setup(20, GPIO.OUT)
+        self._lb_pwm = GPIO.PWM(20, 1000)
+        self._lb_pwm.start(0)
+
+        GPIO.setup(26, GPIO.OUT)
+        self._rb_pwm = GPIO.PWM(26, 1000)
+        self._rb_pwm.start(0)
+
+        GPIO.setup(16, GPIO.OUT)
+        GPIO.setup(6, GPIO.OUT)
+        GPIO.setup(21, GPIO.OUT)
+        GPIO.setup(19, GPIO.OUT)
+        GPIO.output(16, 0)
+        GPIO.output(6, 0)
+        GPIO.output(21, 0)
+        GPIO.output(19, 0)
+
+        self.lfDir      = lambda x: GPIO.output(16, x)
+        self.lfSpeed    = lambda x: self._lf_pwm.ChangeDutyCycle(x)
+        self.rfDir      = lambda x: GPIO.output(6, x)
+        self.rfSpeed    = lambda x: self._rf_pwm.ChangeDutyCycle(x)
+        self.lbDir      = lambda x: GPIO.output(21, x)
+        self.lbSpeed    = lambda x: self._lb_pwm.ChangeDutyCycle(x)
+        self.rbDir      = lambda x: GPIO.output(19, x)
+        self.rbSpeed    = lambda x: self._rb_pwm.ChangeDutyCycle(x)
 
         while self.thread_life:
-            self.left_dir(self.lf > 0)
-            self.left_speed(abs(self.lf * 100))
-            self.right_dir(self.rf > 0)
-            self.right_speed(abs(self.rf * 100))
-            time.sleep(1/update_freq)
+            self.lfDir(self.lf > 0)
+            self.lfSpeed(abs(self.lf * 100))
+            self.rfDir(self.rf > 0)
+            self.rfSpeed(abs(self.rf * 100))
+            self.lbDir(self.lf > 0)
+            self.lbSpeed(abs(self.lb * 100))
+            self.rbDir(self.rf > 0)
+            self.rbSpeed(abs(self.rb * 100))
+            time.sleep(1 / update_freq)
 
 
 class Robot:
