ea9aeb71e1df95b6030a9bcdec4f22e32c06bf92
superfast852 <superfast852@gmail.com>
2023-05-23 21:26:31 -0400

    Fixing possible pinout issues

--- Robots/LidarLess.py
+++ Robots/LidarLess.py
@@ -4,36 +4,36 @@ from networktables import NetworkTables
 
 class LDrive(Drive):  # Override Communications
     def comms(self, com, baud, update_freq=10):
-        GPIO.setup(12, GPIO.OUT)
-        self._lf_pwm = GPIO.PWM(12, 1000)
+        GPIO.setup(20, GPIO.OUT)
+        self._lf_pwm = GPIO.PWM(20, 1000)
         self._lf_pwm.start(0)
 
         GPIO.setup(13, GPIO.OUT)
         self._rf_pwm = GPIO.PWM(13, 1000)
         self._rf_pwm.start(0)
 
-        GPIO.setup(20, GPIO.OUT)
-        self._lb_pwm = GPIO.PWM(20, 1000)
+        GPIO.setup(16, GPIO.OUT)
+        self._lb_pwm = GPIO.PWM(16, 1000)
         self._lb_pwm.start(0)
 
         GPIO.setup(26, GPIO.OUT)
         self._rb_pwm = GPIO.PWM(26, 1000)
         self._rb_pwm.start(0)
 
-        GPIO.setup(16, GPIO.OUT)
+        GPIO.setup(12, GPIO.OUT)
         GPIO.setup(6, GPIO.OUT)
         GPIO.setup(21, GPIO.OUT)
         GPIO.setup(19, GPIO.OUT)
-        GPIO.output(16, 0)
+        GPIO.output(12, 0)
         GPIO.output(6, 0)
         GPIO.output(21, 0)
         GPIO.output(19, 0)
 
-        self.lfDir      = lambda x: GPIO.output(16, x)
+        self.lfDir      = lambda x: GPIO.output(21, x)
         self.lfSpeed    = lambda x: self._lf_pwm.ChangeDutyCycle(x)
         self.rfDir      = lambda x: GPIO.output(6, x)
         self.rfSpeed    = lambda x: self._rf_pwm.ChangeDutyCycle(x)
-        self.lbDir      = lambda x: GPIO.output(21, x)
+        self.lbDir      = lambda x: GPIO.output(12, x)
         self.lbSpeed    = lambda x: self._lb_pwm.ChangeDutyCycle(x)
         self.rbDir      = lambda x: GPIO.output(19, x)
         self.rbSpeed    = lambda x: self._rb_pwm.ChangeDutyCycle(x)
