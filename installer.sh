ef098fdc44212e975a559e45684c7270ec80428b
superfast852 <superfast852@gmail.com>
2023-09-24 01:41:34 -0400

    cleaning up a bit

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
-pip install --upgrade pip || return 1
-git clone https://github.com/simondlevy/BreezySLAM.git
-cd BreezySLAM/python || return 1
+pip install --upgrade pip
+git clone https://github.com/simondlevy/breezyslam.git
+cd breezyslam/python
 python3 setup.py install
 cd ../../Resources/py_install/
 python setup.py install
