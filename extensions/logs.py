32a580c7ec73c44a2b8433fbe0d8aee522046900
superfast852 <superfast852@gmail.com>
2024-06-14 17:00:24 -0400

    prettied up the log started message for timestamping n stuff.

--- extensions/logs.py
+++ extensions/logs.py
@@ -1,6 +1,8 @@
 import logging
+import datetime
+
 
 if not logging.getLogger().hasHandlers():
     logging.basicConfig(filename="log", filemode='a', format=f'%(asctime)s [%(levelname)s]%(message)s', datefmt='%H:%M', level=logging.DEBUG)
-    logging.info(f"\n{'-'*20}Started logging.")
+    logging.info(f"\n{'-'*20}\nStarted logging at {datetime.datetime.now()}\n{'-'*20}\n")
 
