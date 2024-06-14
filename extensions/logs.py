eae4d892aded5911a1a7f9cf9684f6ada1351235
superfast852 <superfast852@gmail.com>
2024-06-14 17:12:52 -0400

    logs.py now creates the logs folder if it does not exist.

--- extensions/logs.py
+++ extensions/logs.py
@@ -1,8 +1,10 @@
 import logging
-import datetime
-
 
 if not logging.getLogger().hasHandlers():
+    import datetime
+    import os
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
     ct = datetime.datetime.now().strftime('%H:%M:%S')
     logging.basicConfig(filename=f"logs/{datetime.date.today()}@{ct}", filemode='a', format=f'%(asctime)s [%(levelname)s]%(message)s', datefmt='%H:%M', level=logging.DEBUG)
     logging.info(f"\n{'-'*20}\nStarted logging at {ct}\n{'-'*20}\n")
